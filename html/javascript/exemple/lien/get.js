726084b18295b677323a9095b7aec9e4d9556fb5
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2020-03-20 11:58:00 +0100

    add

---
+++ html/javascript/exemple/lien/get.js
@@ -0,0 +1,4 @@
+var link = document.getElementById("myLink");
+var href = link.getAttribute("href"); // On récupère l'attribut « href »
+alert(href);
+link.setAttribute("href", "http://www.google.com"); // on édite
---
+++ html/javascript/exemple/lien/id.js
@@ -0,0 +1,4 @@
+var link = document.getElementById("myLink");
+var href = link.href;
+alert(href);
+link.href = "http://www.google.com";
